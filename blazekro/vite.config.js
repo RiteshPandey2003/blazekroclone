925490d27ab3b194b7bf19b01e9a407e0198c71d
Ritesh Pandey <riteshpandey72060@gmail.com>
2024-03-06 10:04:09 +0530

    commit

--- blazekro/vite.config.js
+++ blazekro/vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server : {
-    proxy :{
-      '/api' : 'http://localhost:8001'
-    }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8001',
+        secure: false,
+      },
+    },
   },
   plugins: [react()],
 })
