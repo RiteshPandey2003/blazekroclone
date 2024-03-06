4a8e28feb2f3accbd192d8c815628ddb5a2365a6
Ritesh Pandey <riteshpandey72060@gmail.com>
2024-03-06 14:08:07 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -13,13 +13,19 @@ dotenv.config();
 const __dirname = path.resolve()
 const app = express();
 const port = process.env.PORT || 8001;
-const corsOptions = {
-    origin: ['https://blazekroclone.onrender.com','http://localhost:5173', 'http://localhost:5174'],
-    credentials: true,
-};
 
+app.use(cors());
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "");
+    res.header("Access-Control-ALlow-Methods",
+    "GET", "HEAD", "OPTION", "PUT", "POST", "DELETE"
+    );
+    res.header("Access-Control-Allow-Headers",
+    "Origin, X_Requested-With, Content-Type, Accept, Authorization"
+    )
+  next();
+})
 app.use(express.json());
-app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
 
--- blazekro/vite.config.js
+++ blazekro/vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:8001',
+        changeOrigin: true,
         secure: false,
       },
     },
   },
   plugins: [react()],
-})
+});
