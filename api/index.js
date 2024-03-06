76ea6fe175e1482163b6bfa983be12a48d4f2bc2
Ritesh Pandey <riteshpandey72060@gmail.com>
2024-03-06 14:12:43 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -14,41 +14,39 @@ const __dirname = path.resolve()
 const app = express();
 const port = process.env.PORT || 8001;
 
+// CORS Configuration
 app.use(cors());
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "");
-    res.header("Access-Control-ALlow-Methods",
-    "GET", "HEAD", "OPTION", "PUT", "POST", "DELETE"
-    );
-    res.header("Access-Control-Allow-Headers",
-    "Origin, X_Requested-With, Content-Type, Accept, Authorization"
-    )
-  next();
-})
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://blazekroclone.onrender.com");
+    res.header("Access-Control-Allow-Methods", ["GET", "HEAD", "OPTIONS", "PUT", "POST", "DELETE"]);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
 
+// API Routes
+app.use("/user", userRoute);
+app.use("/product", productRoute);
+app.use("/cart", cartRoute);
 
+// Serve Static Files
+app.use(express.static(path.join(__dirname, "blazekro/dist")));
 
-
-app.listen(port, () => {
-    console.log(`Server is listening at port: ${port}`);
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "blazekro", "dist", "index.html"));
 });
 
+// Connect to MongoDB
 mongoose.connect(process.env.DBURL).then(() => {
     console.log("Connected to the database");
 }).catch((error) => {
     console.log("Error connecting to the database:", error);
 });
 
-app.use("/user", userRoute);
-app.use("/product", productRoute);
-app.use("/cart", cartRoute);
-
-
-app.use(express.static(path.join(__dirname, "blazekro/dist")));
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "blazekro", "dist", "index.html"));
+// Start the Server
+app.listen(port, () => {
+    console.log(`Server is listening at port: ${port}`);
 });
