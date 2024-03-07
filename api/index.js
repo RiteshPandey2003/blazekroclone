849a8bf60cc25fda640636265e735f1b215cdfc3
Ritesh Pandey <riteshpandey72060@gmail.com>
2024-03-07 11:55:19 +0530

    commit

--- api/index.js
+++ api/index.js
@@ -1,21 +1,20 @@
-
 import cookieParser from "cookie-parser";
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
 import productRoute from "./routes/productRoute.js";
 import cartRoute from "./routes/cartRoute.js";
-import path from 'path';
-
+import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 const app = express();
 const port = process.env.PORT || 8001;
 
-
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
@@ -29,17 +28,20 @@ app.use("/cart", cartRoute);
 app.use(express.static(path.join(__dirname, "blazekro/dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "blazekro", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "blazekro", "dist", "index.html"));
 });
 
 // Connect to MongoDB
-mongoose.connect(process.env.DBURL).then(() => {
+mongoose
+  .connect(process.env.DBURL)
+  .then(() => {
     console.log("Connected to the database");
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.log("Error connecting to the database:", error);
-});
+  });
 
 // Start the Server
 app.listen(port, () => {
-    console.log(`Server is listening at port: ${port}`);
+  console.log(`Server is listening at port: ${port}`);
 });
